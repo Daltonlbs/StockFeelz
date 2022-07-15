b4dcce1656e9792b18deff0cf440c10b0200b7c6
Daltonlbs <45320833+Daltonlbs@users.noreply.github.com>
2022-07-14 21:09:04 -0300

    Add files via upload

--- stockFeelz2.py
+++ stockFeelz2.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
-#import pydeck as pdk
 import matplotlib.pyplot as plt
 import investpy as ip
 from datetime import datetime, timedelta
